f0a61e19e6a6158f0d7533d9ab1264ea8a756a53
giorgiabes <44198003+giorgiabes@users.noreply.github.com>
2025-06-21 09:40:58 +0400

    add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
+text = getattr(response, "text", None)
+prompt_token_count = getattr(response.usage_metadata, "prompt_token_count", None)
+candidates_token_count = getattr(
+    response.usage_metadata, "candidates_token_count", None
+)
+
+print(text)
+print(f"Prompt tokens: {prompt_token_count}")
+print(f"Response tokens: {candidates_token_count}")
